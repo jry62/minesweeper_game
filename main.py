bf7636fa229301637c47b61282e0595405e8018e
jry62 <102626853+jry62@users.noreply.github.com>
2022-04-25 18:31:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from email.utils import collapse_rfc2231_value
 from tkinter import *
+from cell import Cell
 import settings, utils
 
 root = Tk()
@@ -10,7 +12,7 @@ root.resizable(False, False)
 
 top_frame = Frame(
     root,
-    bg= 'red', #change later to black
+    bg= 'black', #change later to black
     width=settings.WIDTH,
     height=utils.height_prct(25),
 )
@@ -18,7 +20,7 @@ top_frame.place(x=0, y=0)       # setting the axis for the top frame
 
 left_frame = Frame(
     root,
-    bg= 'blue',  #change later to black
+    bg= 'black',  #change later to black
     width=utils.width_prct(25),
     height=utils.height_prct(75),
 )
@@ -26,7 +28,7 @@ left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
     root,
-    bg="green",      # change to black
+    bg="black",      # change to black
     width= utils.width_prct(75),
     height=utils.height_prct(75),
 )
@@ -34,5 +36,15 @@ center_frame.place(
     x=utils.width_prct(25),
     y=utils.height_prct(25),
 )
+
+for x in range(5):
+    for y in range(5):
+        c = Cell()
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(
+            column=y, 
+            row=x
+        )
+
 # run the window
 root.mainloop()
